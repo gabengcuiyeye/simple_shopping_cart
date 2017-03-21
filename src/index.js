763178fc23646ae995dc4256195493a4a51d0c0a
yunhongyao <hongyaoyun@gmail.com>
2017-03-21 15:46:38 +0800

    Update index.js
    change the improt path for list.vue.  I put all my file in the src.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ Vue.config.devtools = true  //开启devtool调试
 import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
-import list from './components/list.vue'
+import list from '../src/components/list.vue'
 new Vue({
     el: '#app',
     components: {
