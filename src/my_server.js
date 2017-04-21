8da2f968917013f1d52f446b58f787d763653023
yunhongyao <hongyaoyun@gmail.com>
2017-04-21 17:30:44 +0800

    Update my_server.js
    增加了删除功能

--- src/my_server.js
+++ src/my_server.js
@@ -5,17 +5,11 @@ var path = require('path');
 
 var fs = require("fs");
 
-
-
 //express设置
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var router = express.Router();
-
-
 var app = express();
-var server = require('http').createServer(app);
 app.use(express.static(path.join(__dirname, 'public')));//静态文件托管
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.static(path.join(__dirname, 'components')));
@@ -24,7 +18,6 @@ app.use(bodyParser.json({limit: '1mb'}));  //body-parser 解析json格式数据
 app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
     extended: true
 }));
-
 
 
 var conn = mysql.createConnection({
@@ -38,10 +31,6 @@ conn.connect();
 
 var insertSQL = 'insert into t_list(f_name,f_avatar) values("haha","http://g.mdcdn.cn/h5/img/icon_tool/common_detail_alarm_succ.png")';
 var selectSQL = 'select * from t_list limit 4';
-
-var search_result ;
-
-
 
 conn.query(selectSQL, function (err2, rows) {
     if (err2) console.log(err2);
@@ -57,10 +46,8 @@ app.post('/add_to_cart', function (req, res) {
     let response = req.body;
     let query = 'SELECT * FROM t_item_user WHERE f_item_id  = ?';
     let query_param = req.body.item_id;
-
     conn.query(query,query_param, function (err2, rows) {
         if (err2) console.log(err2);
-
         if(rows.length===0){
             //insert
             let sql = 'insert into t_item_user(f_uid,f_item_id) values(?,?)';
@@ -77,7 +64,6 @@ app.post('/add_to_cart', function (req, res) {
                 }
             })
         }else{
-
             let $return={
                 errcode:2,
                 errmsg:'该商品已经存在',
@@ -99,8 +85,6 @@ app.post('/add_to_cart', function (req, res) {
             let user_id=return_data.req.body.user_id;
             let query = 'SELECT * FROM t_item_user WHERE f_uid  = ?';
             let query_param = user_id;
-
-
             return new Promise(function(resolve){
                 conn.query(query,query_param, function (err2, rows) {
                     resolve(rows);
@@ -113,9 +97,6 @@ app.post('/add_to_cart', function (req, res) {
                 for(let i=0,len=rows.length;i<len;i++){
                     whereIn.push(rows[i].f_item_id);
                 }
-                console.log(whereIn);
-
-
                 conn.query(query_item,[whereIn], function (err2, rowss) {//异步分两次查。。。。
                     if (err2){
                         console.log(err2);
@@ -130,8 +111,6 @@ app.post('/add_to_cart', function (req, res) {
                     res.end(JSON.stringify($return));//返回
 
                 });
-
-
             }else if(rows.length==0){
                 let $return={
                     errcode:0,
@@ -143,6 +122,52 @@ app.post('/add_to_cart', function (req, res) {
         });
 
     });
+
+app.post('/delete_item', function (req, res) {
+    let request = req.body;
+        return new Promise(function(resolve){
+            resolve(request);
+        })
+        .then(function (request) {
+            let query = 'SELECT f_item_id FROM t_item_user WHERE f_item_id  = ' + request.item_id + ' limit 1';
+            return new Promise(function (resolve) {
+                conn.query(query, function (err2, rows) {
+                    if (err2) console.log(err2);
+                    var obj = {
+                        id: request.item_id,
+                        row: rows
+                    };
+                    resolve(obj);
+
+                })
+            })
+        })
+        .then(function(obj){
+            console.log(obj.row);
+            if(obj.row.length>0){
+                let query = 'delete  FROM t_item_user WHERE f_item_id  = '+obj.id;
+                conn.query(query, function (err2, rows) {
+                    if(rows.affectedRows==1){
+                        let $return={
+                            errcode:0,
+                            errmsg:'',
+                            data:'删除成功',
+                        };
+                        res.end(JSON.stringify($return));//返回
+                    }
+                })
+            }
+        })
+        .catch(function () {
+            console.log('error');
+            let $return={
+                errcode:1,
+                errmsg:'',
+                data:'删除失败',
+            };
+            res.end(JSON.stringify($return));//返回
+        })
+});
 
 
 var server = app.listen(4444, function () {
