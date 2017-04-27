326edc41129e4a0f0c8bfdc2f919e73ca0c71aeb
yunhongyao <hongyaoyun@gmail.com>
2017-04-27 16:59:03 +0800

    Update my_server.js
    1.把配置部分抽出来setting.js
    2.初始化查询购车用一句sql语句就能完成，之前查了两次，优化了下
    3.传数据给pug模板

--- src/my_server.js
+++ src/my_server.js
@@ -1,28 +1,6 @@
 //require
+let app = require('./setting.js');
 let mysql = require('mysql');
-var express = require('express');
-var path = require('path');
-var fs = require("fs");
-const pug = require('pug');
-
-//express设置
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-var server = require('http').createServer(app);
-app.use(express.static(path.join(__dirname, 'public')));//静态文件托管
-app.use(express.static(path.join(__dirname, '../dist')));
-app.use(express.static(path.join(__dirname, 'components')));
-
-app.use(bodyParser.json({limit: '1mb'}));  //body-parser 解析json格式数据
-app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
-    extended: true
-}));
-
-//模板支持
-app.set('views', path.join(__dirname, './public/view'));
-app.set('view engine', 'pug');//jade升级成了pug
 
 var conn = mysql.createConnection({
     host: 'localhost',
@@ -32,7 +10,7 @@ var conn = mysql.createConnection({
     port: 3306
 });
 conn.connect();
-
+//加入购物车
 app.post('/add_to_cart', function (req, res) {
     let request = req.body;
     let query = 'SELECT f_uid,f_item_id FROM t_item_user WHERE f_item_id  = ?';
@@ -40,6 +18,7 @@ app.post('/add_to_cart', function (req, res) {
 
     conn.query(query,query_param, function (err2, rows) {
         if (err2) console.log(err2);
+
         if(rows.length===0){
             //insert
             let sql = 'insert into t_item_user(f_uid,f_item_id) values(?,?)';
@@ -56,6 +35,7 @@ app.post('/add_to_cart', function (req, res) {
                 }
             })
         }else{
+
             let $return={
                 errcode:2,
                 errmsg:'该商品已经存在',
@@ -65,54 +45,7 @@ app.post('/add_to_cart', function (req, res) {
     })
 });
 
-
-app.post('/search_cart', function (req, res) {
-        return new Promise(function(resolve){
-            let return_data = {
-                req :req
-            };
-            resolve(return_data);
-        })
-        .then(function(return_data){
-            let user_id=return_data.req.body.user_id;
-            let query = 'SELECT * FROM t_item_user WHERE f_uid  = ?';
-            let query_param = user_id;
-            return new Promise(function(resolve){
-                conn.query(query,query_param, function (err2, rows) {
-                    resolve(rows);
-                });
-            });
-        }).then(function(rows){
-            if(rows.length>0){
-
-                let query_item = 'SELECT * FROM t_list WHERE f_id  in (?)';
-                let whereIn = [];
-                for(let i=0,len=rows.length;i<len;i++){
-                    whereIn.push(rows[i].f_item_id);
-                }
-                conn.query(query_item,[whereIn], function (err2, rowss) {//异步分两次查。。。。
-                    if (err2){
-                        console.log(err2);
-                    }
-                    let $return={
-                        errcode:0,
-                        errmsg:'',
-                        data:rowss,
-                    };
-                    res.end(JSON.stringify($return));//返回
-                });
-            }else if(rows.length==0){
-                let $return={
-                    errcode:0,
-                    errmsg:'',
-                    data:''
-                };
-                res.end(JSON.stringify($return));//返回
-            }
-        });
-
-    });
-
+//从购物车删除商品
 app.post('/delete_item', function (req, res) {
     let request = req.body;
     console.log('********************');
@@ -161,24 +94,30 @@ app.post('/delete_item', function (req, res) {
         })
 });
 
+//页面初始化加载，可以把初始化购物车在这查询
 app.get('/',function(req,res){//有index默认进index页，我把index名字改了才进入这个页面的
-    console.log(9999);
-    var selectSQL = 'select * from t_list limit 4';
-    conn.query(selectSQL, function (err2, rows) {
+    let list_query_sql = 'select f_id,f_name,f_avatar from t_list limit 10',
+        list_search_result,
+        cart_search_result;
+
+    conn.query(list_query_sql, function (err2, rows) {
         if (err2) console.log(err2);
-        var seach_result = JSON.stringify(rows);
-        /**********************pug tpl,首屏直出********************/
-        res.render('index',{//pug(jade)是express默认模板
-            init_data:seach_result,
+        list_search_result = JSON.stringify(rows);
+        return new Promise(function(resolve){
+            let cart_query_sql = 'SELECT t_list.f_id,t_list.f_name,t_list.f_avatar FROM t_list INNER JOIN t_item_user ON t_list.f_id= t_item_user.f_item_id WHERE t_item_user.f_uid=2333';
+            conn.query(cart_query_sql, function (err2, rows) {
+                if (err2) console.log(err2);
+                resolve(rows);
+            });
+        }).then(function(rows){
+            cart_search_result = JSON.stringify(rows);
+            res.render('index',{//pug(jade)是express默认模板
+                init_list_data:list_search_result,
+                init_cart_data:cart_search_result,
+            });
         });
     });
 });
-
 var server = app.listen(4444, function () {
     console.log('yes');
-
 });
-
-
-module.exports = router;
-module.exports = app;
