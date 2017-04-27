36f34beb0d2a67731ef418da5d71fe952896ec6c
yunhongyao <hongyaoyun@gmail.com>
2017-04-27 16:53:54 +0800

    Update list.vue
    1.商品列表和购物车初始化在后台做了，不用在页面中发送http请求，减少了请求数量
    2.template用pug模板替换

--- src/components/list.vue
+++ src/components/list.vue
@@ -21,6 +21,7 @@
         margin-bottom:20px;
         img{
             width: 120px;
+            height:120px;
         }
     }
     .content_right{
@@ -57,92 +58,60 @@
         }
     }
 </style>
-<template>
-    <div class="content_wrap">
-        <div class="content_left">
-            <h3>商品列表</h3>
-            <div class="item" v-for="list in mess" @click="add_to_cart" data-id="{{list.f_id}}">
-                <div class="img_wrap">
-                    <img :src='list.f_avatar' >
-                </div>
-                <span>{{list.f_name}}</span>
-            </div>
-        </div>
-        <div class="content_right" id="content_right" @click="delete_item">
-            <h3>购物车</h3>
-            <div class="item" v-for="list in cart_mess" data-id="{{list.f_id}}">
-                <div class="img_wrap">
-                    <img :src='list.f_avatar'>
-                </div>
-                <span>{{list.f_name}}</span>
-                <div class="delete_layer">
-                    <p class="delete">删除</p>
-                </div>
-            </div>
 
-        </div>
-    </div>
+<template lang="pug">
+    div.content_wrap
+        div.content_left
+            h3 商品列表
+            div.item(v-for='list in mess',@click='add_to_cart',data-id='{{list.f_id}}')
+                div.img_wrap
+                    img(:src='list.f_avatar')
+                span {{list.f_name}}
+
+        div#content_right.content_right(@click="delete_item")
+            h3 购物车
+            div.item(v-for='list in cart_mess',data-id='{{list.f_id}}')
+                div.img_wrap
+                    img(:src='list.f_avatar')
+                span {{list.f_name}}
+                div.delete_layer
+                p.delete 删除
 </template>
 <script>
     import Vue from 'vue'
     export default {
-        props: {
-            voting_desc: {
-                type: Number,
-                default: function() {
-                    return{
-                        voting_desc: 0,
-                    }
-                }
-            },
-            teacher_group_id: {
-                type: Number,
-                default: function() {
-                    return{
-                    }
-                }
-            },
-        },
         data(){
             return {
-                mess:init_data,
-                cart_mess:[]
+                mess:list_data,
+                cart_mess:cart_data,
             }
         },
         methods:{
             add_to_cart:function (e) {
                 let item_id = parseInt(e.currentTarget.getAttribute('data-id'));//vue获取当前dom对象
                 let data = {id:2333,item_id:item_id};
-                $.ajax({
-                    type: "post",
-                    url: "/add_to_cart",
-                    dataType: "json",
-                    data: JSON.stringify(data),
-                    contentType: 'application/json',
-                    success: function (response) {
-                        if(response.errcode ===1 ){
-                            console.log('加入购物车成功');
-                            let div = document.createElement('div');
-                            div.setAttribute('class','item');
-                            div.setAttribute('data-id',item_id);
-                            let img_src;
-                            if(e.target.nodeName==='IMG'){
-                                img_src = e.target.getAttribute('src');
-                            }else if(e.target.nodeName==='SPAN'){
-                                img_src = e.target.previousSbiling.getAttribute('src');
-                            }
-                            let str = '<div class="img_wrap"> '+'<img src="'+ img_src +'">'
-                                    + '</div><span>test1</span>'+'<div class="delete_layer">'
-                                +'<p class="delete">删除</p>'+ '</div>';
-                            div.innerHTML = str;
-                            document.getElementById('content_right').appendChild(div);
-                        }else if(response.errcode ===2){
-                            alert('已经添加过了哦');
+                this.$http.post('/add_to_cart',{id:2333,item_id:item_id}).then(response => {
+                    if(response.data.errcode ===1 ){
+                        console.log('加入购物车成功');
+                        let div = document.createElement('div');
+                        div.setAttribute('class','item');
+                        div.setAttribute('data-id',item_id);
+                        let img_src;
+                        if(e.target.nodeName==='IMG'){
+                            img_src = e.target.getAttribute('src');
+                        }else if(e.target.nodeName==='SPAN'){
+                            img_src = e.target.previousSbiling.getAttribute('src');
                         }
-                    },
-                    error: function (request) {
-
+                        let str = '<div class="img_wrap"> '+'<img src="'+ img_src +'">'
+                            + '</div><span>test1</span>'+'<div class="delete_layer">'
+                            +'<p class="delete">删除</p>'+ '</div>';
+                        div.innerHTML = str;
+                        document.getElementById('content_right').appendChild(div);
+                    }else if(response.data.errcode ===2){
+                        alert('已经添加过了哦');
                     }
+                }, response => {
+                    // error callback
                 });
             },
             delete_item:function(e){
@@ -154,23 +123,15 @@
                             parent_dom.parentNode.removeChild(parent_dom);
                         }
                     }, response => {
+
                     });
                 }else{
                     console.log(e.target);
+
                 }
             },
         },
         init:function(){
-            let self = this;
-            //初始化购物车
-            this.$http.post('/search_cart',{user_id:2333}).then(response => {
-                if(response.data.errcode===0){
-                    let data = response.data;
-                    self.cart_mess = data.data;
-                }
-            }, response => {
-                // error callback
-            });
         }
     }
 </script>
